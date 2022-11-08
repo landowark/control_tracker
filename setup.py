7df80d730d6c24330740f7ff6b5d498aa0ea3ded
landowark <lwark@waffles.cscscience.ca>
2022-11-08 09:42:36 -0600

    Rebuilding setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     version='0.1.0',
     py_modules=['controls'],
     install_requires=[
-        'Click', 'PyYAML', 'alembic', 'xlrd', 'XlsxWriter', 'xlwt', 'pandas', 'plotly', 'sqlalchemy', 'zipp'
+        'Click', 'PyYAML', 'alembic', 'xlrd', 'XlsxWriter', 'xlwt', 'pandas', 'plotly', 'sqlalchemy', 'zipp', 'openpyxl'
     ],
     entry_points={
         'console_scripts': [
